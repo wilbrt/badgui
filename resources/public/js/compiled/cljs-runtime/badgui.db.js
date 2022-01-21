e85b4b947dd58755d128c66f487b0ab41103b181
Vilppu Saarinen <vilppusaa@gmail.com>
2022-01-21 13:48:36 +0200

    Removed compiled js

--- resources/public/js/compiled/cljs-runtime/badgui.db.js
+++
@@ -1,4 +0,0 @@
-goog.provide('badgui.db');
-badgui.db.default_db = new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"name","name",1843675177),"re-frame"], null);
-
-//# sourceMappingURL=badgui.db.js.map
--- resources/public/js/compiled/cljs-runtime/shadow.module.app.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('app');
-
-try { badgui.core.init(); } catch (e) { console.error("An error occurred when calling (badgui.core/init)"); throw(e); }
